c9ff9d7cd933543ec177478d40945241f0436bc1
heshamshalaby <hisham_ayman_95@hotmail.com>
2017-08-14 19:16:03 -0400

    PROJECT COMPLETE Y'ALL!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,15 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.R.attr.button;
+import static android.R.id.button2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView ballImage;
+        ballImage = (ImageView) findViewById(R.id.image_eightBall);
+        final int[] ballArray = {R.mipmap.ball1, R.mipmap.ball2, R.mipmap.ball3, R.mipmap.ball4, R.mipmap.ball5 };
+        Button askButt;
+        askButt = (Button) findViewById(R.id.button2);
+        askButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("magiceightball", "Ah Please");
+                Random randomanswergenerator = new Random();
+                int answer = randomanswergenerator.nextInt(5);
+                Log.d("Fuck me", "the number of the variable is: " + answer);
+                ballImage.setImageResource(ballArray[answer]);
+            }
+        });
+
     }
 }
